edf0e3cbd71b070283c75b1246a15c272f2b02b0
dowbennett <dow.bennett@gmail.com>
2019-07-25 15:53:10 -0400

    When to use python over ansible

---
+++ pyvans/simpsonsquote.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import requests
+
+simpson = requests.get("https://thesimpsonsquoteapi.glitch.me/quotes").json()
+with open("simpsons-py.txt", "w") as monorail:
+    monorail.write(simpson[0]["quote"] + "\n" + simpson[0]["character"] + "\n")
