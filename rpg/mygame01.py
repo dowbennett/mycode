8c4a23eed879781e9afbc3d400bc4a0895bff7ef
dowbennett <dow.bennett@gmail.com>
2019-07-25 13:34:04 -0400

    Jinja2 filters

--- rpg/mygame01.py
+++ rpg/mygame01.py
@@ -8,17 +8,18 @@ Commands:
   go [direction]
   get [item]
   help
+  exit (exits the game)
 ''')
 
 def showStatus():
-    print('----------------------------')
+    print('\n----------------------------')
     print('You are in the ' + currentRoom)
     print('Inventory : ' + str(inventory))
     if "item" in rooms[currentRoom] and rooms[currentRoom]['item'] != 'chad':
         print('You see a ' + rooms[currentRoom]['item'])
     if "item" in rooms[currentRoom] and rooms[currentRoom]['item'] == 'chad':
         print('You see Chad Feeser! He is here to help you learn Python! Take \'chad\' with you!')   
-    print('----------------------------')
+    print('----------------------------\n')
 
 inventory = []
 
@@ -64,6 +65,7 @@ rooms = {
             },
         'Pantry' : {
             'south' : 'Kitchen',
+            'east' : 'Dungeon',
             'item' : 'monster'
             }
         }
@@ -82,10 +84,15 @@ while True:
 
 #GO
     if move[0] == 'go':
-        if move[1] in rooms[currentRoom]:
+        if currentRoom == 'Foyer' and move[1] == 'south':
+            print('\nYou don\'t have a key to exit the door!')
+        if currentRoom == 'Pantry' and move[1] == 'east':
+            print('\nYou found a slide into the dungeon!')
+            currentRoom = rooms[currentRoom][move[1]]
+        elif move[1] in rooms[currentRoom]:
             currentRoom = rooms[currentRoom][move[1]]       
         else:
-            print('You can\'t go that way!')
+            print('\nYou can\'t go that way!')
 
 #GET
     if move[0] == 'get':
@@ -94,28 +101,32 @@ while True:
             print(move[1] + ' picked up!')
             del rooms[currentRoom]['item']
         else:
-            print('Can\'t get ' + move[1] + '!')
+            print('\nCan\'t get ' + move[1] + '!')
 
 #HELP
     if move[0] == 'help':
         showInstructions()
 
-#MONSTERDEATH
-    if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item'] and 'sword' not in inventory:
-        print('You open the Pantry. A monster jumps out and eats your soul!  GAME OVER!')
+#EXITTHEGAME
+    if move[0] == 'exit':
+        break
+
+#DEATHBYMONSTER
+    if 'item' in rooms[currentRoom] and rooms[currentRoom]['item'] == 'monster' and 'sword' not in inventory:
+        print('\nYou open the Pantry. A monster jumps out and eats your soul!  GAME OVER!')
         break
 
 #MONSTERDEFEAT
-    if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item'] and 'sword' in inventory:
-        print('You open the Pantry. A monster jumps out! You pull out your sword and defeat the monster.')
+    if 'item' in rooms[currentRoom] and rooms[currentRoom]['item'] == 'monster' and 'sword' in inventory:
+        print('\nYou open the Pantry. A monster jumps out! You pull out your sword and defeat the monster.')
         rooms[currentRoom]['item'] = 'deadmonster'
 
 #SAVE PRINCESS AND CHAD
     if currentRoom == 'Door' and 'princess' in inventory and 'chad' in inventory:
-        print('You escaped the castle with the princess and also saved Chad! YOU WIN!')
+        print('\nYou escaped the castle with the princess and also saved Chad! YOU WIN!')
         break
 
 #SAVE PRINCESS
     if currentRoom == 'Door' and 'princess' in inventory:
-        print('You escaped the house with the castle with the princess! YOU WIN!')
+        print('\nYou escaped the house with the castle with the princess! YOU WIN!')
         break
