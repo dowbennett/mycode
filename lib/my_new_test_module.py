3607c7145fea57cc2d78ff677d431b946b463c2a
dowbennett <dow.bennett@gmail.com>
2019-07-26 12:02:31 -0400

    final

---
+++ lib/my_new_test_module.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+ANSIBLE_METADATA = {
+        'metadata_version' : '1.1',
+        'status' : ['preview'],
+        'supported_by' : 'community'
+        }
+
+DOCUMENTATION = '''
+---
+module: my_new_test_module
+
+short_description: This is my sample module
+
+version_added: "2.4"
+
+description: 
+    - "This is my longer description explaining my sample module"
+
+options:
+    name:
+        descriptions:
+            - This is the message to send to the sampl module
+        required: true
+    new:
+        description:
+            - Control to demo if the result of this module is changed or not
+        required: false
+    author:
+        - Dow Bennett
+'''
+
+EXAMPLES = '''
+#Pass in a message
+- name: Test with a message
+  my_new_test_module:
+    name: hello world
+
+#Pass in message and have changed true
+- name: Test with a message and changed output
+  my_new_test_module:
+    name: hello world
+    new: true
+
+#Fail the module
+- name: Test failure of the module
+  my_new_test_module:
+    name: fail me
+'''
+
+RETURN = '''
+Original_message:
+    description: The original name param that was passed in
+    type: str
+message:
+    description: The output message that the sample module generates
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+def run_module():
+    module_args = dict(
+        name=dict(type='str', required=True),
+        new=dict(type='bool', required=False, default=False),
+        combine=dict(type='bool', required=False, default=False)
+    )
+
+    result = dict(
+        changed=False,
+        combined=False,
+        original_message='',
+        message=''
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    if module.check_mode:
+        return result
+
+    result['original_message'] = module.params['name']
+    result['message'] = 'goodbye'
+
+    if module.params['new']:
+        result['changed'] = True
+
+    if module.params['name'] == 'who you gonna call':
+        result['message'] = 'GHOSTBUSTERS'
+
+    if module.params['name'] == 'ping':
+        result['message'] = 'pong'
+
+    if module.params['combine']:
+        result['combined'] = True
+        result['message'] = result['original_message'] + " " + result['message'] 
+
+    if module.params['name'] == 'fail me':
+        module.fail_json(msg='You requested this to fail', **result)
+
+    module.exit_json(**result)
+
+def main():
+    run_module()
+
+if __name__ == '__main__':
+    main()
---
+++ xmen/uncannyapi.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import argparse
+import time
+import requests
+import hashlib
+
+XAVIER = 'http://gateway.marvel.com/v1/public/characters'
+
+def hashbuilder(timeywimey, pvkee, pubkee):
+    return hashlib.md5((timeywimey+pvkee+pubkee).encode('utf-8')).hexdigest()
+
+def marvelcharcall(stampystamp, hashyhash, pkeyz, lookmeup):
+    r = requests.get(XAVIER+"?name="+lookmeup+"&ts="+stampystamp+"&apikey="+pkeyz+"&hash="+hashyhash)
+    print(XAVIER+"?name="+lookmeup+"&ts="+stampystamp+"&apikey="+pkeyz+"&hash="+hashyhash)
+    return r.json()
+
+def main():
+    with open(args.dev) as mccoy:
+        beast = mccoy.read().rstrip('\n')
+
+    with open(args.pub) as munroe:
+        storm = munroe.read().rstrip('\n')
+
+    nightcrawler = str(time.time()).rstrip('.')
+
+    cerebro = hashbuilder(nightcrawler, beast, storm)
+
+    uncannyxmen = marvelcharcall(nightcrawler, cerebro, storm, "Wolverine")
+
+    print(uncannyxmen)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dev', help='Provide the /path/to/file.priv containing Marvel private developer key')
+    parser.add_argument('--pub', help='Provide the /path/to/file.pub containing Marvel public developer key')
+    args = parser.parse_args()
+    main()
